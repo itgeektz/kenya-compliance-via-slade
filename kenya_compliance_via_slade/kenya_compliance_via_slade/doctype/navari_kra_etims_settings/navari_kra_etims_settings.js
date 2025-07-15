30d8602e86c6fdc9f92eb0c224b2126dba4d1174
muruthigitau <muruthigitau@gmail.com>
2025-07-15 14:48:03 +0300

    refactor: remove deprecated mode of payment submission code in Navari KRA eTims Settings
    Cleans up deprecated logic for submitting mode of payments
    
    Removes commented-out and unused code blocks related to mode of payment submission from the `Navari KRA eTims Settings` module.
    
    This cleanup eliminates deprecated logic, improves maintainability, and ensures the codebase reflects current behavior and standards.

--- kenya_compliance_via_slade/kenya_compliance_via_slade/doctype/navari_kra_etims_settings/navari_kra_etims_settings.js
+++ kenya_compliance_via_slade/kenya_compliance_via_slade/doctype/navari_kra_etims_settings/navari_kra_etims_settings.js
@@ -123,22 +123,22 @@ frappe.ui.form.on("Navari KRA eTims Settings", {
         __("eTims Actions")
       );
 
-      frm.add_custom_button(
-        __("Submit Mode of Payments"),
-        function () {
-          frappe.call({
-            method:
-              "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.send_all_mode_of_payments",
-            args: { settings_name: frm.doc.name },
+      // frm.add_custom_button(
+      //   __("Submit Mode of Payments"),
+      //   function () {
+      //     frappe.call({
+      //       method:
+      //         "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.send_all_mode_of_payments",
+      //       args: { settings_name: frm.doc.name },
 
-            callback: (response) => {},
-            error: (error) => {
-              // Error Handling is Defered to the Server
-            },
-          });
-        },
-        __("eTims Actions")
-      );
+      //       callback: (response) => {},
+      //       error: (error) => {
+      //         // Error Handling is Defered to the Server
+      //       },
+      //     });
+      //   },
+      //   __("eTims Actions")
+      // );
     }
 
     frm.add_custom_button(
