eeaaeddc339238ba04dcef4f71a8ffecf4687a94
maniamartial <martialmania19@gmail.com>
2025-08-23 11:08:20 +0300

    fix:restrict settings retrieval to mapped companies only

--- kenya_compliance_via_slade/kenya_compliance_via_slade/utils.py
+++ kenya_compliance_via_slade/kenya_compliance_via_slade/utils.py
@@ -312,9 +312,10 @@ def get_settings(company_name: str = None, branch_id: str = None, settings_name:
         
     company_name = (
         company_name
-        or frappe.defaults.get_user_default("Company")
-        or frappe.get_value("Company", {}, "name")
+        # or frappe.defaults.get_user_default("Company")
+        # or frappe.get_value("Company", {}, "name")
     )
+    
     if frappe.db.exists(
         ORGANISATION_MAPPING_DOCTYPE_NAME, 
         {"company": company_name, "is_active": 1}
@@ -328,16 +329,18 @@ def get_settings(company_name: str = None, branch_id: str = None, settings_name:
         if mapping and mapping.parent:
             return frappe.get_doc(SETTINGS_DOCTYPE_NAME, mapping.parent).as_dict()
     
-    if frappe.db.exists(SETTINGS_DOCTYPE_NAME, {"is_active": 1}):
-        settings = frappe.db.get_value(
-            SETTINGS_DOCTYPE_NAME,
-            {"is_active": 1},
-            "*",
-            as_dict=True,
-        )
-        return settings
+    # if frappe.db.exists(SETTINGS_DOCTYPE_NAME, {"is_active": 1}):
+    #     settings = frappe.db.get_value(
+    #         SETTINGS_DOCTYPE_NAME,
+    #         {"is_active": 1},
+    #         "*",
+    #         as_dict=True,
+    #     )
+    #     frappe.throw(str(settings))
+    #     return settings
     
     return None
+
 
 
 def get_branch_id(company_name: str, vendor: str) -> str | None:
