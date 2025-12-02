e5ee5612649ea45068a92803cdbe265fdb2f2ea9
muruthigitau <muruthigitau@gmail.com>
2025-12-02 10:10:32 +0300

    feat(ui): Enhance client-side actions for Registered Purchases

--- kenya_compliance_via_slade/kenya_compliance_via_slade/doctype/navari_etims_registered_purchases/navari_etims_registered_purchases.js
+++ kenya_compliance_via_slade/kenya_compliance_via_slade/doctype/navari_etims_registered_purchases/navari_etims_registered_purchases.js
@@ -1,5 +1,4 @@
 // Copyright (c) 2024, Navari Ltd and contributors
-// For license information, please see license.txt
 const doctypeName = "Navari eTims Registered Purchases";
 
 frappe.ui.form.on(doctypeName, {
@@ -14,7 +13,7 @@ frappe.ui.form.on(doctypeName, {
           fields: ["name"],
           limit_page_length: 1,
         },
-        callback: function (response) {
+        async callback(response) {
           if (response.message && response.message.length > 0) {
             companyName = response.message[0].name;
           }
@@ -22,123 +21,209 @@ frappe.ui.form.on(doctypeName, {
       });
     }
 
-    if (!frm.is_new()) {
-      frm.add_custom_button(
-        __("Create Supplier"),
-        function () {
-          frappe.call({
-            method:
-              "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.create_supplier_from_fetched_registered_purchases",
-            args: {
-              request_data: {
-                name: frm.doc.name,
-                company_name: companyName,
-                supplier_name: frm.doc.supplier_name,
-                supplier_pin: frm.doc.supplier_pin,
-                supplier_branch_id: frm.doc.supplier_branch_id,
+    if (frm.is_new()) return;
+
+    Promise.all([
+      frappe.db.get_list("Supplier", {
+        filters: { supplier_name: frm.doc.supplier_name },
+        fields: ["name"],
+        limit: 1,
+      }),
+      frappe.db.get_list("Purchase Invoice", {
+        filters: [
+          ["custom_source_registered_purchase", "=", frm.doc.name],
+          ["docstatus", "in", [0, 1]],
+        ],
+        fields: ["name"],
+        limit: 1,
+      }),
+    ]).then(async ([suppliers, invoices]) => {
+      const supplierExists = suppliers.length > 0;
+      const invoiceExists = invoices.length > 0;
+
+      if (!supplierExists) {
+        frm.add_custom_button(
+          __("Create Supplier"),
+          function () {
+            frappe.call({
+              method:
+                "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.create_supplier_from_fetched_registered_purchases",
+              args: {
+                request_data: {
+                  name: frm.doc.name,
+                  company_name: companyName,
+                  supplier_name: frm.doc.supplier_name,
+                  supplier_pin: frm.doc.supplier_pin,
+                  supplier_branch_id: frm.doc.supplier_branch_id,
+                },
               },
-            },
-            callback: (response) => {},
-            error: (error) => {
-              // Error Handling is Defered to the Server
-            },
-          });
-        },
-        __("eTims Actions")
-      );
-      frm.add_custom_button(
-        __("Create Items"),
-        function () {
-          frappe.call({
-            method:
-              "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.create_items_from_fetched_registered",
-            args: {
-              request_data: {
-                name: frm.doc.name,
-                company_name: companyName,
-                items: frm.doc.items,
+              freeze: true,
+              freeze_message: __("Creating Supplier..."),
+              callback: (response) => {
+                const newSupplier = response.message;
+                if (newSupplier) {
+                  frappe.set_route("Form", "Supplier", newSupplier.name);
+                } else {
+                  frappe.msgprint(
+                    __("Failed to create supplier. Please try again.")
+                  );
+                }
               },
-            },
-            callback: (response) => {},
-            error: (error) => {
-              // Error Handling is Defered to the Server
-            },
-          });
-        },
-        __("eTims Actions")
-      );
-      frm.add_custom_button(
-        __("Create Purchase Invoice"),
-        function () {
-          frappe.call({
-            method:
-              "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.create_purchase_invoice_from_request",
-            args: {
-              request_data: {
-                name: frm.doc.name,
-                company_name: companyName,
-                supplier_name: frm.doc.supplier_name,
-                supplier_pin: frm.doc.supplier_pin,
-                supplier_branch_id: frm.doc.supplier_branch_id,
-                branch: frm.doc.branch,
-                organisation: frm.doc.organisation,
-                supplier_invoice_no: frm.doc.supplier_invoice_number,
-                supplier_invoice_date: frm.doc.sales_date,
-                items: frm.doc.items,
+              error: (error) => {
+                frappe.msgprint(
+                  __(
+                    "An error occurred while creating the supplier. Please try again.",
+                    error
+                  )
+                );
               },
-            },
-            callback: (response) => {},
-            error: (error) => {
-              // Error Handling is Defered to the Server
-            },
-          });
-        },
-        __("eTims Actions")
+            });
+          },
+          __("eTims Actions")
+        );
+      }
+
+      const itemNames = frm.doc.items.map((i) => i.item_name);
+      const existingItems = await frappe.db.get_list("Item", {
+        filters: [["item_name", "in", itemNames]],
+        fields: ["item_name", "name"],
+      });
+      const existingItemNames = existingItems.map((i) => i.item_name);
+
+      const missingItems = frm.doc.items.filter(
+        (i) => !existingItemNames.includes(i.item_name)
       );
 
-      frm.add_custom_button(
-        __("Fetch Registered Purchase Details"),
-        function () {
-          frappe.call({
-            method:
-              "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.perform_purchase_search",
-            args: {
-              request_data: {
-                id: frm.doc.name,
-                document_name: frm.doc.name,
-                company_name: companyName,
+      if (missingItems.length > 0) {
+        frm.add_custom_button(
+          __("Create Missing Items"),
+          function () {
+            frappe.call({
+              method:
+                "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.create_items_from_fetched_registered",
+              args: {
+                request_data: {
+                  name: frm.doc.name,
+                  company_name: companyName,
+                  items: missingItems,
+                },
               },
-            },
-            callback: (response) => {},
-            error: (error) => {
-              // Error Handling is Defered to the Server
-            },
-          });
-        },
-        __("eTims Actions")
-      );
+              freeze: true,
+              freeze_message: __("Creating Missing Items..."),
+              callback: (response) => {
+                const result = response.message;
+                if (result) {
+                  const createdItems = result.created || [];
+                  const errors = result.errors || [];
+
+                  let msg = "";
+
+                  if (createdItems.length) {
+                    msg += "<b>Created Items:</b><br>";
+                    createdItems.forEach((itemName) => {
+                      const link = frappe.urllib.get_full_url(
+                        `/app/item/${encodeURIComponent(itemName)}`
+                      );
+                      msg += `<a href="${link}" target="_blank">${itemName}</a><br>`;
+                    });
+                  }
+
+                  if (errors.length) {
+                    msg += "<br><b>Errors:</b><br>";
+                    errors.forEach((err) => {
+                      msg += `${err.item}: ${err.error}<br>`;
+                    });
+                  }
+
+                  frappe.msgprint(msg);
+                  frm.reload_doc();
+                } else {
+                  frappe.msgprint(
+                    __("Failed to create missing items. Please try again.")
+                  );
+                }
+              },
+              error: (error) => {
+                frappe.msgprint(
+                  __(
+                    "An error occurred while creating the missing items. Please try again."
+                  )
+                );
+              },
+            });
+          },
+          __("eTims Actions")
+        );
+      }
+
+      if (
+        !invoiceExists &&
+        supplierExists &&
+        existingItemNames.length === itemNames.length
+      ) {
+        frm.add_custom_button(
+          __("Create Purchase Invoice"),
+          function () {
+            frappe.call({
+              method:
+                "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.create_purchase_invoice_from_request",
+              args: {
+                request_data: {
+                  name: frm.doc.name,
+                  company_name: companyName,
+                  supplier_name: frm.doc.supplier_name,
+                  supplier_pin: frm.doc.supplier_pin,
+                  supplier_branch_id: frm.doc.supplier_branch_id,
+                  branch: frm.doc.branch,
+                  organisation: frm.doc.organisation,
+                  supplier_invoice_no: frm.doc.supplier_invoice_number,
+                  supplier_invoice_date: frm.doc.sales_date,
+                  items: frm.doc.items,
+                },
+              },
+              freeze: true,
+              freeze_message: __("Creating Purchase Invoice..."),
+              callback: (response) => {
+                const newInvoice = response.message;
+                if (newInvoice) {
+                  frappe.set_route("Form", "Purchase Invoice", newInvoice.name);
+                } else {
+                  frappe.msgprint(
+                    __("Failed to create purchase invoice. Please try again.")
+                  );
+                }
+              },
+              error: (error) => {
+                frappe.msgprint(
+                  __(
+                    "An error occurred while creating the purchase invoice. Please try again.",
+                    error
+                  )
+                );
+              },
+            });
+          },
+          __("eTims Actions")
+        );
+      }
+
       // frm.add_custom_button(
-      //   __("Create Purchase Receipt"),
+      //   __("Fetch Registered Purchase Details"),
       //   function () {
       //     frappe.call({
-      //       method: null,
+      //       method:
+      //         "kenya_compliance_via_slade.kenya_compliance_via_slade.apis.apis.perform_purchase_search",
       //       args: {
       //         request_data: {
-      //           name: frm.doc.name,
+      //           id: frm.doc.name,
+      //           document_name: frm.doc.name,
       //           company_name: companyName,
-      //           supplier_name: frm.doc.supplier_name,
-      //           supplier_pin: frm.doc.supplier_pin,
-      //           items: frm.doc.items,
       //         },
-      //       },
-      //       callback: (response) => {},
-      //       error: (error) => {
-      //         // Error Handling is Defered to the Server
       //       },
       //     });
       //   },
       //   __("eTims Actions")
       // );
-    }
+    });
   },
 });
